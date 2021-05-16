39c9f14cba9df0ec61779a7399c6ce6e55cfc737
Jan Wołowiec <wolowiec.jan7@gmail.com>
2021-05-16 18:33:53 +0200

    TB updates

--- sim/behav_tb.sv
+++ sim/behav_tb.sv
@@ -16,22 +16,22 @@ module behav_tb();
     reg signed [(M+N)-1:0] input_vertices[];
     reg signed [M-1:0] transformed_vertices[];
     int frame = 0;
-    
+
     tv_loader TV_LOADER();
 
     rasterize_behav restarizer_i(framebuffer, transformed_vertices);
     vertex_processor_behav vertex_processor_i(transform_matrix, input_vertices, transformed_vertices);
 
-    initial begin     
-        TV_LOADER.load_signed_int("../../../../../sim/test_vector_int.txt");  
+    initial begin
+        TV_LOADER.load_signed_int("test_vector_int.txt");
         for (int i = 0; i < TV_LOADER.tv_int.size(); i++) begin
             input_vertices = new[input_vertices.size() + 1](input_vertices);
             input_vertices[input_vertices.size() - 1] = TV_LOADER.tv_int[i];
-        end                
+        end
         for (int i = 0; i < 1; i++) begin
-            vertex_processor_i.process();   
+            vertex_processor_i.process();
             restarizer_i.rasterize(transformed_vertices.size());
-            TV_LOADER.save_bmp_file($sformatf("behav_test_result_picture_%0d.bmp", i), framebuffer);
+            TV_LOADER.save_bmp_file($sformatf("../../../../../sim/results/behav_tb_image_%0d.bmp", i), framebuffer);
             for (int x = 0; x < transformed_vertices.size(); x++) begin
                 $display("%d", transformed_vertices[x]);
                 input_vertices[x] = transformed_vertices[x] <<< N;
--- sim/rtl_tb.sv
+++ sim/rtl_tb.sv
@@ -12,11 +12,11 @@ parameter N = 7;
 wire  signed [M-1:0]   output_vertex;
 wire                   output_vertex_valid;
 
-reg signed [M+N-1:0] transform_matrix [0:15] = {     
+reg signed [M+N-1:0] transform_matrix [0:15] = {
     83, -48, -83, 0,
     34, 118, -34, 0,
     90, 0, 90, 0,
-    0, 0, 0, 128         
+    0, 0, 0, 128
 };
 reg signed [(N+M)-1:0] input_vertex = 0;
 reg signed [(N+M)-1:0] input_vertex_counter = 0;
@@ -66,18 +66,18 @@ rasterize_behav restarizer_i(framebuffer, transformed_vertices);
 
 initial begin
     reset = 1;
-    TV_LOADER.load_signed_int("../../../../../sim/test_vector_int.txt");  
+    TV_LOADER.load_signed_int("test_vector_int.txt");
     for (int i = 0; i < TV_LOADER.tv_int.size(); i++) begin
         input_vertices = new[input_vertices.size() + 1](input_vertices);
         input_vertices[input_vertices.size() - 1] = TV_LOADER.tv_int[i];
-    end  
+    end
     #100 reset = 0;
-    
+
     @(negedge output_vertex_valid);
-    
+
     restarizer_i.rasterize(transformed_vertices.size());
-    TV_LOADER.save_bmp_file("rtl_tb_image.bmp", framebuffer);
-    
+    TV_LOADER.save_bmp_file("../../../../../sim/results/rtl_tb_image.bmp", framebuffer);
+
      $stop;
 end
 
