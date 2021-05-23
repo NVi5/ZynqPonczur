8465bcd10e665648dd04630a8b396f9d508a98ab
Jan Wołowiec <wolowiec.jan7@gmail.com>
2021-05-23 22:11:04 +0200

    Remove header

--- src/gpu_wrapper_vhdl.vhd
+++ src/gpu_wrapper_vhdl.vhd
@@ -1,14 +1,3 @@
--- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
--- --------------------------------------------------------------------------------
--- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
--- Date        : Tue May 18 16:23:06 2021
--- Host        : RYZEN-PC running 64-bit major release  (build 9200)
--- Command     : write_vhdl -force -mode synth_stub
---               C:/Users/nvade/OneDrive/Dokumenty/Workspace/Xilinx/ZynqSDUP/src/main/ip/main_gpu_wrapper_0_0/main_gpu_wrapper_0_0_stub.vhdl
--- Design      : main_gpu_wrapper_0_0
--- Purpose     : Stub declaration of top-level module interface
--- Device      : xc7z010clg400-1
--- --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
